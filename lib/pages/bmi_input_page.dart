fc625002eec564f67b0dd9f88322265d14451dc4
allan.urbiztondo <allan.urbiztondo@lingarogroup.com>
2023-11-19 19:34:54 +0800

    perf: improve card iteration

--- lib/pages/bmi_input_page.dart
+++ lib/pages/bmi_input_page.dart
@@ -20,60 +20,96 @@ class _BmiInputPageState extends State<BmiInputPage> {
     });
   }
 
-  final List<List<Map<String, dynamic>>> rowsCardData = [
-    [
-      {
-        'iconLabel': 'Male',
-        'iconData': FontAwesomeIcons.mars,
-      },
-      {
-        'iconLabel': 'Female',
-        'iconData': FontAwesomeIcons.venus,
-      },
-    ],
-    [
-      {
-        'iconLabel': 'Male',
-        'iconData': FontAwesomeIcons.mars,
-      },
-    ],
-    [
-      {
-        'iconLabel': 'Male',
-        'iconData': FontAwesomeIcons.mars,
-      },
-      {
-        'iconLabel': 'Male',
-        'iconData': FontAwesomeIcons.mars,
-      },
-    ],
+  final List<Map<String, List<Map<String, dynamic>>>> rowsCardData = [
+    {
+      'top': [
+        {
+          'iconLabel': 'Male',
+          'iconData': FontAwesomeIcons.mars,
+        },
+        {
+          'iconLabel': 'Female',
+          'iconData': FontAwesomeIcons.venus,
+        },
+      ],
+      'middle': [
+        {
+          'iconLabel': 'Height',
+          'iconData': FontAwesomeIcons.mars,
+        },
+      ],
+      'bottom': [
+        {
+          'iconLabel': 'Weight',
+          'iconData': FontAwesomeIcons.mars,
+        },
+        {
+          'iconLabel': 'Age',
+          'iconData': FontAwesomeIcons.mars,
+        },
+      ],
+    }
   ];
 
   @override
   Widget build(BuildContext context) {
-    int cardIndex = -1;
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('BMI CALCULATOR')),
       ),
       body: Column(
-        children: rowsCardData.map((rowCardData) {
-          return Expanded(
-            child: Row(
-              children: rowCardData.asMap().entries.map((entry) {
-                final Map<String, dynamic> cardData = entry.value;
-                final int rowCardIndex = ++cardIndex;
-                return ExpandedCard(
-                  cardColor: selectedIndex == rowCardIndex ? kActiveCardColor : kDefaultCardColor,
-                  iconLabel: cardData['iconLabel'],
-                  iconData: cardData['iconData'],
-                  onTap: () {
-                    updateSelectedIndex(rowCardIndex);
-                  },
-                );
-              }).toList(),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: rowsCardData.expand((rowCardData) {
+          return [
+            Expanded(
+              child: Row(
+                children: rowCardData['top']!.map<Widget>((data) {
+                  final int rowCardIndex = rowCardData['top']!.indexOf(data);
+                  print(rowCardIndex);
+                  return ExpandedCard(
+                    cardColor: selectedIndex == rowCardIndex ? kActiveCardColor : kDefaultCardColor,
+                    iconLabel: data['iconLabel']!,
+                    iconData: data['iconData']!,
+                    onTap: () {
+                      updateSelectedIndex(rowCardIndex);
+                    },
+                  );
+                }).toList(),
+              ),
             ),
-          );
+            Expanded(
+              child: Row(
+                children: rowCardData['middle']!.map<Widget>((data) {
+                  final int rowCardIndex = rowCardData['middle']!.indexOf(data) + rowCardData['top']!.length;
+                  print(rowCardIndex);
+                  return ExpandedCard(
+                    cardColor: selectedIndex == rowCardIndex ? kActiveCardColor : kDefaultCardColor,
+                    iconLabel: data['iconLabel']!,
+                    iconData: data['iconData']!,
+                    onTap: () {
+                      updateSelectedIndex(rowCardIndex);
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: rowCardData['bottom']!.map<Widget>((data) {
+                  final int rowCardIndex = rowCardData['bottom']!.indexOf(data) + rowCardData['top']!.length + rowCardData['middle']!.length;
+                  print(rowCardIndex);
+                  return ExpandedCard(
+                    cardColor: selectedIndex == rowCardIndex ? kActiveCardColor : kDefaultCardColor,
+                    iconLabel: data['iconLabel']!,
+                    iconData: data['iconData']!,
+                    onTap: () {
+                      updateSelectedIndex(rowCardIndex);
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+          ];
         }).toList(),
       ),
       bottomNavigationBar: Container(
